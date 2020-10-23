46e0fc4d2b26c6112bc1a411d6c9665883467ede
enoxime <4358598+Enoxime@users.noreply.github.com>
2020-10-22 20:10:06 -0400

    Add ssl email option and check boolean value

--- build/taiga-back/docker.py
+++ build/taiga-back/docker.py
@@ -8,6 +8,14 @@ def is_env_exists(environment_variable: str, default = ''):
     else:
         return content
 
+def is_env_bool_exists(environment_variable: str, default = False):
+    content = os.getenv(environment_variable)
+
+    if content == True:
+        return True
+    else:
+        return False
+
 MEDIA_URL = is_env_exists("MEDIA_URL")
 STATIC_URL = is_env_exists("STATIC_URL")
 SITES_FRONT_SCHEME = is_env_exists("SITES_FRONT_SCHEME")
@@ -21,7 +29,7 @@ PUBLIC_REGISTER_ENABLED = is_env_exists("PUBLIC_REGISTER_ENABLED")
 DEFAULT_FROM_EMAIL = is_env_exists("DEFAULT_FROM_EMAIL")
 SERVER_EMAIL = is_env_exists("SERVER_EMAIL")
 
-CELERY_ENABLED = is_env_exists("CELERY_ENABLED", True)
+CELERY_ENABLED = is_env_bool_exists("CELERY_ENABLED", True)
 
 EVENTS_PUSH_BACKEND = is_env_exists("EVENTS_PUSH_BACKEND")
 EVENTS_PUSH_BACKEND_OPTIONS = is_env_exists("EVENTS_PUSH_BACKEND_OPTIONS")
@@ -30,7 +38,8 @@ EVENTS_PUSH_BACKEND_OPTIONS = {"url": "amqp://taiga:taiga@rabbitmq:5672/taiga"}
 # Uncomment and populate with proper connection parameters
 # for enable email sending. EMAIL_HOST_USER should end by @domain.tld
 EMAIL_BACKEND = is_env_exists("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
-EMAIL_USE_TLS = is_env_exists("EMAIL_USE_TLS", False)
+EMAIL_USE_TLS = is_env_bool_exists("EMAIL_USE_TLS", False)
+EMAIL_USE_SSL = is_env_bool_exists("EMAIL_USE_SSL", False)
 EMAIL_HOST = is_env_exists("EMAIL_HOST", "localhost")
 EMAIL_HOST_USER = is_env_exists("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = is_env_exists("EMAIL_HOST_PASSWORD")
